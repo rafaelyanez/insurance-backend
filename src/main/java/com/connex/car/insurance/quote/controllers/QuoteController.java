c758f3b1ae81d2a66b513575668fa94be1b64454
Rafael Yanez <rafaelyanez@gmail.com>
2023-12-10 15:08:46 -0500

    add test Get method for the quote endpoint

--- src/main/java/com/connex/car/insurance/quote/controllers/QuoteController.java
+++ src/main/java/com/connex/car/insurance/quote/controllers/QuoteController.java
@@ -3,6 +3,7 @@ package com.connex.car.insurance.quote.controllers;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,16 @@ public class QuoteController {
     private static final Logger log = LogManager.getLogger(QuoteController.class);
 
     @PostMapping("/quote")
-    @CrossOrigin(origins = "http://localhost:5173")
+    @CrossOrigin(origins = "https://frontend-dot-insurance-407608.uk.r.appspot.com")
     public Quote quote(@RequestBody QuoteData quoteData) {
         log.info("Quote Data from the client ==> " + quoteData);
 
+        return new Quote(1399, "RD1314234", true);
+    }
+
+    @GetMapping("/quote")
+    @CrossOrigin(origins = "https://frontend-dot-insurance-407608.uk.r.appspot.com")
+    public Quote testQuote() {
         return new Quote(1399, "RD1314234", true);
     }
 }
